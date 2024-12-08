baed1eedfce341e4815052482777925fb6b15144
gobalkrishnan <gobalkrishnan.books@gmail.com>
2024-12-08 12:36:22 +0530

    first commit

---
+++ main.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+
+import 'content.dart';
+
+void createDir(dirpath) {
+  // print(dirpath);
+  dirpath = replace(dirpath);
+
+  var directory = Directory(dirpath);
+  if (!directory.existsSync()) {
+    directory.createSync(recursive: true);
+  }
+}
+
+String replace(value) {
+  return value
+      .toString()
+      .replaceAll(",", "_")
+      .replaceAll(" ", "_")
+      .replaceAll("?", "_")
+      .replaceAll(".", "_")
+      .replaceAll(":", "_");
+}
+
+void createFile(key, value, filepath) {
+  filepath = replace(filepath);
+
+  var route = "${filepath}/readme.md";
+  if (route[0] == "/") {
+    route = route.toString().replaceAll("/", "");
+  }
+
+  //print(route);
+
+  var file = File(route);
+  if (!file.existsSync()) {
+    file.createSync(recursive: true);
+  }
+
+  if (key != "text" && key != "li" && key != "ul") {
+    if (file.existsSync()) {
+      var wtr = file.readAsStringSync() + "$key \n";
+      file.writeAsStringSync(wtr);
+    }
+  } else {
+    if (file.existsSync() && value != null && value.isNotEmpty) {
+      var read = "";
+      if (key == "text") {
+        read = file.readAsStringSync() + "\n > ${value} ";
+      } else if (key == "ul") {
+        read = file.readAsStringSync() + "\n - ${value} \n";
+      }
+      file.writeAsStringSync(read);
+    }
+  }
+}
+
+void createFileEmptyDir(key, value, filepath) {
+  filepath = replace(filepath);
+  key = replace(key);
+
+  var route = "${filepath}/$key/readme.md";
+  print(route);
+  var file = File(route);
+  if (!file.existsSync()) {
+    file.createSync(recursive: true);
+  }
+}
+
+var rootpathdir = [];
+
+void formly(data) {
+  if (data is Map) {
+    for (var entry in data.entries) {
+      if (entry.value.isNotEmpty) {
+        var routefile = rootpathdir.join("/");
+        createFile(entry.key, entry.value, routefile);
+
+        if (entry.key != "text" && entry.key != "li" && entry.key != "ul") {
+          rootpathdir.add(entry.key);
+        }
+
+        var routedir = rootpathdir.join("/");
+        createDir(routedir);
+        formly(entry.value);
+        rootpathdir.remove(entry.key);
+      } else {
+        var routefile = rootpathdir.join("/");
+        createFile(entry.key, entry.value, routefile);
+
+        createFileEmptyDir(entry.key, entry.value, routefile);
+        var routedir = rootpathdir.join("/") + "/${entry.key}";
+        createDir(routedir);
+      }
+    }
+  } else if (data is List) {
+    for (var element in data) {
+      formly(element);
+    }
+  }
+}
+
+void main() {
+  formly(listheader);
+}
---
+++ preface_to_the_second_edition/new_to_the_second_edition.dart
@@ -0,0 +1,38 @@
+var p1 = """
+ Based on the suggestions from students and faculty members, this edition has been updated and 
+revised to increase the clarity of presentation where required. Some of the prominent changes 
+are as follows: 
+""";
+
+var p2 = [
+  {
+    "ul":
+        """New sections on omega and theta notations, multi-linked lists, forests, conversion of 
+general trees into binary trees, 2-3 trees, binary heap implementation of priority queues, 
+interpolation search, jump search, tree sort, bucket hashing, cylinder surface indexing"""
+  },
+  {
+    "ul": """
+Additional C programs on header linked lists, parentheses checking, evaluation of prefix 
+expressions, priority queues, multiple queues, tree sort, file handling , address calculation 
+sort
+"""
+  },
+  {
+    "ul": """
+New appendices on dynamic memory allocation, garbage collection, backtracking, 
+Johnson’s problem
+"""
+  },
+  {
+    "ul": """
+Stacks and queues and multi-way search trees are now covered in separate chapters with 
+a more comprehensive explanation of concepts and applications
+"""
+  }
+];
+
+var new_to_the_second_edition = [
+  {"text": p1},
+  {"li": p2}
+];
