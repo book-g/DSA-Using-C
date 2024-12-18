a4b79d869a327fe0894567d3960a431d810b25c6
gobalkrishnan <gobalkrishnan.books@gmail.com>
2024-12-19 04:26:53 +0530

    gobal

---
+++ linked_list/introduction/linked_lists_verus_arrays.dart
@@ -0,0 +1,29 @@
+var p1 = """
+ Both arrays and linked lists are a linear collection of data elements. But unlike an array, a linked 
+list does not store its nodes in consecutive memory locations. Another point of difference between 
+an array and a linked list is that a linked list does not allow random access of data. Nodes in a 
+linked list can be accessed only in a sequential manner. But like an array, insertions and deletions 
+can be done at any point in the list in a constant time.
+""";
+
+var p2 = """
+Another advantage of a linked list over an array is that we can add any number of elements in the 
+list. This is not possible in case of an array. For example, if we declare an array as int marks[20], 
+then the array can store a maximum of 20 data elements only. There is no such restriction in case 
+of a linked list.
+""";
+
+var i3 = "/image/list/4.png";
+
+var p4 = """
+Thus, linked lists provide an efficient way of storing related data and performing basic operations 
+such as insertion, deletion, and updation of information at the cost of extra space required for 
+storing the address of next nodes.
+""";
+
+var ll_vs_arr = [
+  {"text": p1},
+  {"text": p2},
+  {"image": i3},
+  {"text": p4},
+];
---
+++ preface_to_the_first_edition/about_the_book.dart
@@ -0,0 +1,47 @@
+var p1 = """
+This book is aimed at serving as a textbook for undergraduate engineering students of computer 
+science and postgraduate level courses of computer applications. The objective of this book is to 
+introduce the concepts of data structures and apply these concepts in problem solving. The book 
+provides a thorough and comprehensive coverage of the fundamentals of data structures and the 
+principles of algorithm analysis. The main focus has been to explain the principles required to 
+select or design the data structure that will best solve the problem.
+""";
+
+var p2 = """
+ A structured approach is followed to explain the process of problem solving. A theoretical 
+description of the problem is followed by the underlying technique. These are then ably supported 
+by an example followed by an algorithm, and finally the corresponding program in C language.
+""";
+
+var p3 = """
+ The salient features of the book include:""";
+
+var p4 = [
+  {"ul": "Explanation of the concepts using diagrams"},
+  {"ul": "Numerous solved examples within the chapters"},
+  {"ul": "Glossary of important terms at the end of each chapter"},
+  {"ul": "Comprehensive exercises at the end of each chapter"},
+  {"ul": "Practical implementation of the algorithms using tested C programs"},
+  {
+    "ul":
+        "Objective type questions to enhance the analytical ability of the students"
+  },
+  {
+    "ul":
+        "Annexures to provide supplementary information to help generate further interest in the subject"
+  },
+];
+
+var p5 = """
+The book is also useful as a reference and resource to young researchers working on efficient 
+data storage and related applications, who will find it to be a helpful guide to the newly established 
+techniques of a rapidly growing research field.
+""";
+
+var about_the_book = [
+  {"text": p1},
+  {"text": p2},
+  {"text": p3},
+  {"li": p4},
+  {"text": p5},
+];
