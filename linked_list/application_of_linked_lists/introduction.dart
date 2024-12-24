a7a8926afe7a40149f3cf6d858f2f9900acfce3d
Gobal Krishnan V <gobalkrishnan.books@gmail.com>
2024-12-25 05:01:37 +0530

    Delete linked_list directory

--- linked_list/application_of_linked_lists/introduction.dart
+++
@@ -1,9 +0,0 @@
-var p1 = """
-Linked lists can be used to represent polynomials and the different operations that can be performed 
-on them. In this section, we will see how polynomials are represented in the memory using linked 
-lists.
-""";
-
-var application_of_linked_lists_intro = [
-  {"text": p1}
-];
